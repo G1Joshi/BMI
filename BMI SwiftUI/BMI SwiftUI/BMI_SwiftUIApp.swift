f8da3bfd53f349b39ceab9bdd142f79ab45b4062
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-10 18:02:32 +0530

    initial commit

---
+++ BMI SwiftUI/BMI SwiftUI/BMI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_SwiftUIApp.swift
+//  BMI SwiftUI
+//
+//  Created by Jeevan Chandra Joshi on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BMI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
